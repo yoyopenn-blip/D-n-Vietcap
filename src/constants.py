6bff8c0ea2a9eee8e410a1981b44dae0da09d789
Cao Tri <yoyopenn@gmail.com>
2026-06-12 10:08:52 +0700

    Add files via upload

---
+++ src/constants.py
@@ -0,0 +1,24 @@
+# File: src/constants.py
+
+# Dictionary dùng để việt hoá tên nhóm ngành (GICS/TRBC Sectors)
+SECTOR_TRANSLATION = {
+    "Financials": "Tài chính",
+    "Materials": "Nguyên vật liệu",
+    "Industrials": "Công nghiệp",
+    "Consumer Discretionary": "Hàng tiêu dùng không thiết yếu",
+    "Consumer Staples": "Hàng tiêu dùng thiết yếu",
+    "Health Care": "Chăm sóc sức khỏe",
+    "Energy": "Năng lượng",
+    "Utilities": "Tiện ích công cộng",
+    "Information Technology": "Công nghệ thông tin",
+    "Real Estate": "Bất động sản",
+    "Communication Services": "Dịch vụ truyền thông",
+    "Technology": "Công nghệ",
+    "Investment Banking & Brokerage": "Chứng khoán",
+    "Banks": "Ngân hàng",
+    "Real Estate Rental, Development & Operations": "Bất động sản",
+    "Food Processing": "Thực phẩm",
+    "Construction & Engineering": "Xây dựng",
+    "Construction Materials": "Vật liệu xây dựng",
+    "Independent Power Producers": "Sản xuất điện"
+}
